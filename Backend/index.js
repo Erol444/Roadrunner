9313f864fac890a664a2db2db3874185b0e41f1d
Erol444 <erol123444@gmail.com>
2020-11-08 06:01:37 +0100

    fix (BE) fixed file uploading

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 var cors = require('cors')
-app.use(cors())
 var multer  = require('multer');
-var storage = multer.diskStorage({
-destination: function (req, file, cb) {
-    cb(null, 'uploads')
-},
-filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + '.jpg')
-}
-})
-var upload = multer({ storage: storage })
+
+app.use(cors())
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './temp');
+    },
+    filename: (req, file, cb) => {
+      const ext = file.mimetype.split('/')[1];
+      cb(null, `image-${Date.now()}.${ext}`);
+    }
+  });
+
+const upload = multer({ storage: multerStorage })
 
 // Persistent data please
 var events = [];
@@ -28,12 +31,13 @@ app.get('/score', (req,res) => {
 });
 
 // Image upload/download
-app.post('/image', upload.single('image'), async (req, res) => {
+app.post('/postimage', upload.single('image'), (req, res) => {
     events.push({lat: req.body.lat, long: req.body.long, img: req.file.filename});
     res.send('Image was uploaded');
 });
+
 app.get('/image', (req, res) => {
-    var filePath = __dirname + '/uploads/'+req.query.id;
+    var filePath = __dirname + '/temp/'+req.query.id;
     console.log(filePath);
     res.download(filePath)
 });
